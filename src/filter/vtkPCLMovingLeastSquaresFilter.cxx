bbd6c1063c31de28e815871496a184e68095f9c7
Mike Rye <mike.rye@kitware.com>
2019-02-01 18:27:14 +0100

    Added MovingLeastSquares filter.

---
+++ src/filter/vtkPCLMovingLeastSquaresFilter.cxx
@@ -0,0 +1,169 @@
+//==============================================================================
+//
+// Copyright 2012-2018 Kitware, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//==============================================================================
+
+#include "vtkPCLMovingLeastSquaresFilter.h"
+#include "vtkPCLConversions.h"
+#include "vtkObjectFactory.h"
+
+#define PCL_NO_PRECOMPILE 1
+#include <pcl/surface/mls.h>
+
+//------------------------------------------------------------------------------
+// The indices must correspond to the ones in the proxy.
+template <typename PointType, typename NormalType>
+typename pcl::MovingLeastSquares<PointType,NormalType>::UpsamplingMethod
+getUpsamplingMethod(unsigned int index)
+{
+	switch (index)
+	{
+		case 1:
+			return pcl::MovingLeastSquares<PointType,NormalType>::DISTINCT_CLOUD;
+		case 2:
+			return pcl::MovingLeastSquares<PointType,NormalType>::SAMPLE_LOCAL_PLANE;
+		case 3:
+			return pcl::MovingLeastSquares<PointType,NormalType>::RANDOM_UNIFORM_DENSITY;
+		case 4:
+			return pcl::MovingLeastSquares<PointType,NormalType>::VOXEL_GRID_DILATION;
+		case 0:
+		default:
+			return pcl::MovingLeastSquares<PointType,NormalType>::NONE;
+	}
+}
+
+//------------------------------------------------------------------------------
+// The indices must correspond to the ones in the proxy.
+pcl::MLSResult::ProjectionMethod
+getProjectionMethod(unsigned int index)
+{
+	switch (index)
+	{
+		case 1:
+			return pcl::MLSResult::SIMPLE;
+		case 2:
+			return pcl::MLSResult::ORTHOGONAL;
+		case 0:
+		default:
+			return pcl::MLSResult::NONE;
+	}
+}
+
+//------------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPCLMovingLeastSquaresFilter);
+
+//------------------------------------------------------------------------------
+vtkPCLMovingLeastSquaresFilter::vtkPCLMovingLeastSquaresFilter()
+{
+}
+
+//------------------------------------------------------------------------------
+vtkPCLMovingLeastSquaresFilter::~vtkPCLMovingLeastSquaresFilter()
+{
+}
+
+//------------------------------------------------------------------------------
+void vtkPCLMovingLeastSquaresFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//------------------------------------------------------------------------------
+int vtkPCLMovingLeastSquaresFilter::ApplyPCLFilter(
+  vtkPolyData * input,
+  vtkPolyData * output
+)
+{
+  int index = vtkPCLConversions::GetPointTypeIndex(input);
+#define _statement(PointType) return this->InternalApplyPCLFilter<PointType>(input, output);
+  PCLP_INVOKE_WITH_PCL_XYZ_POINT_TYPE(index, _statement)
+#undef _statement
+  return 0;
+}
+
+//------------------------------------------------------------------------------
+// Apply the filter to any point type.
+template <typename PointType>
+int vtkPCLMovingLeastSquaresFilter::InternalApplyPCLFilter(
+  vtkPolyData * input,
+  vtkPolyData * output
+)
+{
+  if (this->ComputeNormals)
+  {
+    std::set<std::string> requiredFieldNames { "normal_x", "normal_y", "normal_z" };
+    PointType const point;
+    int index = vtkPCLConversions::GetPointTypeIndex<PointType const &>(point, requiredFieldNames);
+#define _statement(NormalPointType) return this->InternalInternalApplyPCLFilter<PointType, NormalPointType>(input, output);
+    PCLP_INVOKE_WITH_XYZ_NORMAL_POINT_TYPE(index, _statement)
+#undef _statement
+    return 0;
+  }
+  else
+  {
+    return this->InternalInternalApplyPCLFilter<PointType, PointType>(input, output);
+  }
+}
+
+template <typename InPointType, typename OutPointType>
+int vtkPCLMovingLeastSquaresFilter::InternalInternalApplyPCLFilter(
+  vtkPolyData * input,
+  vtkPolyData * output
+)
+{
+  // For convenience, typedef the cloud type based on the template point type.
+	// typedef pcl::Normal NormalPointType;
+
+  typedef pcl::PointCloud<InPointType> InCloudT;
+  typedef pcl::PointCloud<OutPointType> OutCloudT;
+  // typedef pcl::PointCloud<NormalPointType> NormalCloudT;
+
+  typename InCloudT::Ptr inputCloud(new InCloudT);
+  typename OutCloudT::Ptr outputCloud(new OutCloudT);
+  // typename NormalCloudT::Ptr normalCloud(new NormalCloudT);
+
+  vtkPCLConversions::PointCloudFromPolyData(input, inputCloud);
+
+	// pcl::MovingLeastSquares<InPointType, NormalPointType> mls;
+	pcl::MovingLeastSquares<InPointType, OutPointType> mls;
+	mls.setUpsamplingMethod(
+    getUpsamplingMethod<InPointType, OutPointType>(
+      this->UpsamplingMethod
+    )
+  );
+	mls.setProjectionMethod(getProjectionMethod(this->ProjectionMethod));
+
+	mls.setInputCloud(inputCloud);
+	// mls.setOutputNormals(normalCloud);
+
+	mls.setComputeNormals(this->ComputeNormals);
+	mls.setPolynomialOrder(this->PolynomialOrder);
+	// mls.setPolynomialFit(this->PolynomialFit);
+	mls.setSearchRadius(this->SearchRadius);
+  mls.setSqrGaussParam(this->SqrGaussParam);
+  mls.setUpsamplingRadius(this->UpsamplingRadius);
+  mls.setUpsamplingStepSize(this->UpsamplingStepSize);
+  mls.setPointDensity(this->PointDensity);
+  mls.setDilationVoxelSize(this->DilationVoxelSize);
+  mls.setDilationIterations(this->DilationIterations);
+  mls.setCacheMLSResults(this->CacheMLSResults);
+  mls.setNumberOfThreads(this->NumberOfThreads);
+
+  mls.process((* outputCloud));
+
+  vtkPCLConversions::PolyDataFromPointCloud(outputCloud, output);
+  return 1;
+}
+
---
+++ src/filter/vtkPCLMovingLeastSquaresFilter.h
@@ -0,0 +1,126 @@
+//=============================================================================
+//
+// Copyright 2012-2018 Kitware, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//=============================================================================
+
+#ifndef vtkPCLMovingLeastSquaresFilter_h
+#define vtkPCLMovingLeastSquaresFilter_h
+
+#include "vtkPCLFilter.h"
+
+// TODO
+// Make this a Filter2 class with optional input for the distinct cloud.
+class VTK_EXPORT vtkPCLMovingLeastSquaresFilter : public vtkPCLFilter
+{
+//------------------------------------------------------------------------------
+// Boilerplate VTK code.
+public:
+  static vtkPCLMovingLeastSquaresFilter * New();
+  vtkTypeMacro(vtkPCLMovingLeastSquaresFilter, vtkPCLFilter);
+  void PrintSelf(ostream & os, vtkIndent indent) override;
+
+protected:
+
+  vtkPCLMovingLeastSquaresFilter();
+  ~vtkPCLMovingLeastSquaresFilter();
+
+private:
+  vtkPCLMovingLeastSquaresFilter(const vtkPCLMovingLeastSquaresFilter&) = delete;
+  void operator=(const vtkPCLMovingLeastSquaresFilter&) = delete;
+
+//------------------------------------------------------------------------------
+private:
+  // Keep these values in sync with the defaults in pcl/surface/mls.h.
+  bool         ComputeNormals         = false;
+  int          PolynomialOrder        = 2;
+  // bool         PolynomialFit          = false;
+  double       SearchRadius           = 0.0;
+  double       SqrGaussParam          = 0.0;
+  double       UpsamplingRadius       = 0.0;
+  double       UpsamplingStepSize     = 0.0;
+  int          PointDensity           = 0;
+  float        DilationVoxelSize      = 1.0;
+  int          DilationIterations     = 0;
+  bool         CacheMLSResults        = true;
+  unsigned int NumberOfThreads        = 1;
+
+  unsigned int UpsamplingMethod = 0;
+	unsigned int ProjectionMethod = 1;
+
+public:
+  vtkSetMacro(ComputeNormals, bool);
+  vtkGetMacro(ComputeNormals, bool);
+
+  vtkSetMacro(PolynomialOrder, int);
+  vtkGetMacro(PolynomialOrder, int);
+
+  // vtkSetMacro(PolynomialFit, bool);
+  // vtkGetMacro(PolynomialFit, bool);
+
+  vtkSetMacro(SearchRadius, double);
+  vtkGetMacro(SearchRadius, double);
+
+  vtkSetMacro(SqrGaussParam, double);
+  vtkGetMacro(SqrGaussParam, double);
+
+  vtkSetMacro(UpsamplingRadius, double);
+  vtkGetMacro(UpsamplingRadius, double);
+
+  vtkSetMacro(UpsamplingStepSize, double);
+  vtkGetMacro(UpsamplingStepSize, double);
+
+  vtkSetMacro(PointDensity, int);
+  vtkGetMacro(PointDensity, int);
+
+  vtkSetMacro(DilationVoxelSize, float);
+  vtkGetMacro(DilationVoxelSize, float);
+
+  vtkSetMacro(DilationIterations, int);
+  vtkGetMacro(DilationIterations, int);
+
+  vtkSetMacro(CacheMLSResults, bool);
+  vtkGetMacro(CacheMLSResults, bool);
+
+  vtkSetMacro(NumberOfThreads, unsigned int);
+  vtkGetMacro(NumberOfThreads, unsigned int);
+
+  vtkSetMacro(UpsamplingMethod, unsigned int);
+  vtkGetMacro(UpsamplingMethod, unsigned int);
+
+  vtkSetMacro(ProjectionMethod, unsigned int);
+  vtkGetMacro(ProjectionMethod, unsigned int);
+
+//------------------------------------------------------------------------------
+private:
+  int ApplyPCLFilter(
+    vtkPolyData * input,
+    vtkPolyData * output
+  ) override;
+
+  template <typename PointType>
+  int InternalApplyPCLFilter(
+    vtkPolyData * input,
+    vtkPolyData * output
+  );
+
+  template <typename InPointType, typename OutPointType>
+  int InternalInternalApplyPCLFilter(
+    vtkPolyData * input,
+    vtkPolyData * output
+  );
+
+};
+#endif // vtkPCLMovingLeastSquaresFilter_h
+
